7c886b66152a7dfd25bdfcc80f23558d21820775
mmmhdp <malcevnik99@gmail.com>
2025-09-15 17:36:44 +0300

    feat: produce a under-base cache structure

--- calg/caching/include/cache.h
+++ calg/caching/include/cache.h
@@ -1,2 +1,3 @@
 #pragma once
 
+typedef struct cache CACHE;
--- calg/caching/src/cache.c
+++ calg/caching/src/cache.c
@@ -1,2 +1,39 @@
 #include "cache.h"
+#include "hshtbl.h"
+#include "llst.h"
 
+#include <stdlib.h>
+
+typedef enum
+{
+  LRU,
+  ARC,
+  LFU,
+  LIRS,
+  LRU_2,
+  SLRU,
+  RRIP,
+  Q2
+} CACHE_T;
+
+typedef struct cache
+{
+  CACHE_T cache_t;
+  int cache_sz;
+  HSHTBL *hst;
+  LLST *llst;
+} CACHE;
+
+CACHE *
+cache_init (CACHE_T cache_t, int cache_sz)
+{
+  CACHE *ch = malloc (sizeof (CACHE));
+  ch->cache_t = cache_t;
+  ch->cache_sz = cache_sz;
+  return ch;
+}
+
+void
+cache_free (CACHE_T cache_t, int cache_sz)
+{
+}
