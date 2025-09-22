4fabe8256132239a022f689b973cfb19feb645ec
mmmhdp <malcevnik99@gmail.com>
2025-09-22 16:30:11 +0300

    feat: update infr for comfortable separate compilation

--- calg/caching/scripts/run_tests.sh
+++ calg/caching/scripts/run_tests.sh
@@ -1,5 +1,11 @@
 #! /usr/bin/env bash
 
-for file in ../build/tests/*; do
-	./${file}
-done
+#for file in ../build/tests/*; do
+#	./${file}
+#done
+
+l_file="../build/tests/test_list"
+h_file="../build/tests/test_hshtbl"
+c_file="../build/tests/test_cache"
+
+./${l_file}
