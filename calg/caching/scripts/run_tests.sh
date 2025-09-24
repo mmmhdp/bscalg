91bbfed851266b11d5ea16bbb24d0205fc2c7bcc
mmmhdp <malcevnik99@gmail.com>
2025-09-24 16:08:47 +0300

    feat & fix: massive refactoring of Makefile and scripts. Fix tracking headers too.

--- calg/caching/scripts/run_tests.sh
+++ calg/caching/scripts/run_tests.sh
@@ -1,11 +1,27 @@
 #! /usr/bin/env bash
 
-#for file in ../build/tests/*; do
-#	./${file}
-#done
+test_list () {
+	./${test_bin_dir}/test_list
+}
 
-l_file="../build/tests/test_list"
-h_file="../build/tests/test_hshtbl"
-c_file="../build/tests/test_cache"
+test_hshtbl () {
+	./${test_bin_dir}/test_hshtbl
+}
 
-./${l_file}
+test_cache () {
+	./${test_bin_dir}/test_cache
+}
+
+while getopts "alhcb:": flag; do 
+	case ${flag} in 
+		a)test_list
+		  test_hshtbl
+		  test_cache
+		  ;;
+		l) test_list;;
+		h) test_hshtbl;;
+		c) test_cache;;
+		b) test_bin_dir=${OPTARG};;
+		*) ;;
+	esac
+done
