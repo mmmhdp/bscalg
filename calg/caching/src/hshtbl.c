a9e76956b4ee3dd5e4f373bed6ee79d8c8ff9daa
mmmhdp <malcevnik99@gmail.com>
2025-10-02 17:46:39 +0300

    feat: implement key-value attachment functionality
    Implement ht_add() with a bunch of supporting functions
    for hsh_key, hsh_value and hsh_tbl objects
    in memory management and visial representation areas.

--- calg/caching/src/hshtbl.c
+++ calg/caching/src/hshtbl.c
@@ -11,6 +11,27 @@
 #define HSH_A 123
 #define HSH_B 23
 
+static void line_node_data_printer_with_int_inner_value (NODE_DATA *d,
+                                                         int is_top_node);
+
+static void val_copy (void **dst, void *v, size_t v_sz);
+
+static void hline_update_last_key (HSH_LINE *hl, HSH_KEY *k);
+
+static void ht_add_k_v_pair (HSH_TBL *ht, HSH_KEY *k, HSH_VAL *v);
+
+static HSH_KEY *key_init (char *key, unsigned long key_len);
+
+static HSH_VAL *val_init (void *value, size_t v_sz);
+
+static void key_free (HSH_KEY *k);
+
+static void val_free (HSH_VAL *v);
+
+static void hline_node_free (HSH_LINE_NODE_DATA *d);
+
+static void hline_free (HSH_LINE *hl);
+
 typedef struct hsh_val
 {
   void *val;
@@ -29,6 +50,7 @@ typedef struct hsh_line
   HSH_KEY *last_inserted_key;
 } HSH_LINE;
 
+typedef LIST LINE;
 typedef NODE_DATA HSH_LINE_NODE_DATA;
 
 typedef struct hsh_tbl
@@ -38,7 +60,7 @@ typedef struct hsh_tbl
 } HSH_TBL;
 
 int
-ht_hsf (HSH_TBL *ht, HSH_KEY *k)
+hsf (HSH_TBL *ht, HSH_KEY *k)
 {
   unsigned long i;
   int hs;
@@ -75,26 +97,62 @@ ht_init (int ht_sz)
   return ht;
 }
 
+static HSH_KEY *
+key_init (char *key, unsigned long key_len)
+{
+  HSH_KEY *k;
+
+  k = calloc (1, sizeof (HSH_KEY));
+  k->key = malloc (key_len + 1);
+  key[key_len] = '\0';
+  strcpy (k->key, key);
+
+  k->key_len = key_len;
+
+  return k;
+}
+
+static HSH_VAL *
+val_init (void *value, size_t v_sz)
+{
+  HSH_VAL *v;
+
+  v = calloc (1, sizeof (HSH_VAL));
+  v->val = malloc (v_sz);
+  memcpy (v->val, value, v_sz);
+
+  v->sz = v_sz;
+
+  return v;
+}
+
 static void
-ht_val_free (HSH_VAL *v)
+key_free (HSH_KEY *k)
+{
+  free (k->key);
+  free (k);
+}
+
+static void
+val_free (HSH_VAL *v)
 {
   free (v->val);
   free (v);
 }
 
 static void
-ht_hline_node_free (HSH_LINE_NODE_DATA *d)
+hline_node_free (HSH_LINE_NODE_DATA *d)
 {
   void *v;
   v = list_node_data_get_value (d);
-  ht_val_free (v);
+  val_free (v);
 }
 
 static void
-ht_free_hline (HSH_LINE *hl)
+hline_free (HSH_LINE *hl)
 {
-  free (hl->last_inserted_key);
-  list_free_by_caller (hl->line, ht_hline_node_free);
+  key_free (hl->last_inserted_key);
+  list_free_by_caller (hl->line, hline_node_free);
   free (hl);
 }
 
@@ -104,8 +162,99 @@ ht_free (HSH_TBL *ht)
   int i;
 
   for (i = 0; i < ht->capacity; i++)
-    ht_free_hline (ht->hlines[i]);
+    hline_free (ht->hlines[i]);
 
   free (ht->hlines);
   free (ht);
+}
+
+static void
+val_copy (void **dst, void *v, size_t v_sz)
+{
+  HSH_VAL *tv, *tdst;
+  tv = v;
+  tdst = *dst;
+
+  tdst = malloc (v_sz);
+  memcpy (tdst, v, v_sz);
+
+  tdst->val = malloc (tv->sz);
+  memcpy (tdst->val, tv->val, tv->sz);
+}
+
+static void
+hline_update_last_key (HSH_LINE *hl, HSH_KEY *k)
+{
+  key_free (hl->last_inserted_key);
+  hl->last_inserted_key = key_init (k->key, k->key_len);
+}
+
+static void
+ht_add_k_v_pair (HSH_TBL *ht, HSH_KEY *k, HSH_VAL *v)
+{
+  int hash = hsf (ht, k);
+
+  HSH_LINE *hl = ht->hlines[hash];
+  list_add_node_by_caller (hl->line, v, sizeof (HSH_VAL), val_copy);
+
+  hline_update_last_key (hl, k);
+}
+
+void
+ht_add (HSH_TBL *ht, char *key, unsigned long key_len, void *value,
+        size_t v_sz)
+{
+  HSH_KEY *k;
+  HSH_VAL *v;
+
+  k = key_init (key, key_len);
+  v = val_init (value, v_sz);
+
+  ht_add_k_v_pair (ht, k, v);
+
+  key_free (k);
+  val_free (v);
+}
+
+static void
+line_node_data_printer_with_int_inner_value (NODE_DATA *d, int is_top_node)
+{
+  void *v;
+  HSH_VAL *tv;
+  int value;
+
+  if (d == NULL)
+    {
+      printf ("(NULL node)");
+    }
+
+  v = list_node_data_get_value (d);
+  tv = (HSH_VAL *)v;
+  value = *((int *)tv);
+
+  if (is_top_node)
+    {
+      printf ("(Top node val: %d)->", value);
+    }
+  else
+    {
+      printf ("(Node val: %d)->", value);
+    }
+}
+
+void
+ht_print (HSH_TBL *ht)
+{
+  int i;
+  HSH_LINE *hl;
+
+  for (i = 0; i < ht->capacity; i++)
+    {
+      hl = ht->hlines[i];
+      printf ("Key: [%s] >-<", hl->last_inserted_key->key);
+      list_print (hl->line, line_node_data_printer_with_int_inner_value);
+      printf ("\n");
+    }
+
+  printf ("\n");
 }
