135c839f8ed2b3c1622ab74135d3b33fd6089b50
mmmhdp <malcevnik99@gmail.com>
2025-10-08 17:58:52 +0300

    feat: update main to match test cases

--- calg/caching/src/main.c
+++ calg/caching/src/main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "cache.h"
+
+// #define VERBOSE
+
 int
 main (void)
 {
+  CACHE *c;
   int i, tmp, cache_hit_cnt, nitems, cache_sz, q_sz, *q;
+  void *value;
+  size_t value_sz;
 
   cache_hit_cnt = 0;
   nitems = scanf ("%d", &cache_sz);
@@ -24,13 +31,31 @@ main (void)
       q[i] = tmp;
     }
 
+  c = ch_init (cache_sz);
+  ch_reset_stats (c);
+
+  value_sz = sizeof (int);
   for (i = 0; i < q_sz; i++)
     {
-      printf ("%d ", q[i]);
+      value = (void *)(q + i);
+      ch_put (c, value, value_sz);
     }
-  printf ("\n");
-  printf ("Cache Hits cnt: %d\n", cache_hit_cnt);
 
+  cache_hit_cnt = ch_get_hits (c);
+
+#ifdef VERBOSE
+  int cache_miss_cnt, cache_tries_cnt;
+
+  cache_miss_cnt = ch_get_misses (c);
+  cache_tries_cnt = ch_get_tries (c);
+
+  printf ("Current cache_size %d\n", cache_sz);
+  printf ("misses [%d] hits [%d] with [%d] calls with nq [%d]\n",
+          cache_miss_cnt, cache_hit_cnt, cache_tries_cnt, q_sz);
+#endif
+  printf ("%d", cache_hit_cnt);
+
+  ch_free (c);
   free (q);
 
   return 0;
